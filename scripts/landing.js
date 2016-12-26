82a4f502125aac40da5c30e6aa14345fca75a816
Vincent Tsang <vstsang@gmail.com>
2016-12-26 16:32:54 +0800

    Checkpoint 9 DOM: Events - completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,15 +1,31 @@
- var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function(points) { 
  
-     var points = document.getElementsByClassName('point');
- 
-     var revealPoint = function(index) {
-	 	points[index].style.opacity = 1;
-       	points[index].style.transform = "scaleX(1) translateY(0)";
+	var revealPoint = function(index) {
+ 		points[index].style.opacity = 1;
+    	points[index].style.transform = "scaleX(1) translateY(0)";
        	points[index].style.msTransform = "scaleX(1) translateY(0)";
        	points[index].style.WebkitTransform = "scaleX(1) translateY(0)";			      
-	 };
+	};
   
 	for (i=0; i<points.length; i++) {
     	revealPoint(i);
 	}
  };
+
+window.onload = function() {
+	// Automatically animate the points on a tall screen where scrolling can't trigger the animation
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+	
+	var sellingPoints = document.getElementsByClassName('selling-points')[0];
+	var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+	
+	window.addEventListener('scroll', function(event) {
+    	if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+    	    animatePoints(pointsArray);   
+    	}
+	});
+}
