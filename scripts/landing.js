506b4d2a51a930ffbd79021aba5d72983e37d770
Vincent Tsang <vstsang@gmail.com>
2016-12-26 17:32:32 +0800

    Assignment 9 DOM: Events - completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,19 +1,16 @@
 var pointsArray = document.getElementsByClassName('point');
 
-var animatePoints = function(points) { 
- 
-	var revealPoint = function(index) {
- 		points[index].style.opacity = 1;
-    	points[index].style.transform = "scaleX(1) translateY(0)";
-       	points[index].style.msTransform = "scaleX(1) translateY(0)";
-       	points[index].style.WebkitTransform = "scaleX(1) translateY(0)";			      
-	};
-  
-	for (i=0; i<points.length; i++) {
-    	revealPoint(i);
-	}
- };
+var revealPoint = function(point) {
+	point.style.opacity = 1;
+	point.style.transform = "scaleX(1) translateY(0)";
+	point.style.msTransform = "scaleX(1) translateY(0)";
+	point.style.WebkitTransform = "scaleX(1) translateY(0)";			      
+};
 
+var animatePoints = function(points) { 
+	forEach(points, revealPoint);	
+};
+	
 window.onload = function() {
 	// Automatically animate the points on a tall screen where scrolling can't trigger the animation
     if (window.innerHeight > 950) {
