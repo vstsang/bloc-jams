e5afdce3d2dad41e8e1c26aa96482c661c8c8480
Vincent Tsang <vstsang@gmail.com>
2017-01-10 10:47:42 +0800

    Checkpoint 17: jQuery Collection and Album Views - completed

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,28 +1,29 @@
-var collectionItemTemplate =
-	'<div class="collection-album-container column fourth">'
-	+ '  <img src="assets/images/album_covers/01.png"/>'
-	+ '  <div class="collection-album-info caption">'
-	+ '    <p>'
-	+ '      <a class="album-name" href="album.html"> The Colors </a>'
-	+ '      <br/>'
-	+ '      <a href="album.html"> Pablo Picasso </a>'
-	+ '      <br/>'
-	+ '      X songs'
-	+ '      <br/>'
-	+ '    </p>'
-	+ '  </div>'
-	+ '</div>'
-	;
+var buildCollectionItemTemplate = function() {
+	var template =
+		'<div class="collection-album-container column fourth">'
+		+ '  <img src="assets/images/album_covers/01.png"/>'
+		+ '  <div class="collection-album-info caption">'
+		+ '    <p>'
+		+ '      <a class="album-name" href="album.html"> The Colors </a>'
+		+ '      <br/>'
+		+ '      <a href="album.html"> Pablo Picasso </a>'
+		+ '      <br/>'
+		+ '      X songs'
+		+ '      <br/>'
+		+ '    </p>'
+		+ '  </div>'
+		+ '</div>'
+		;
+	
+	return $(template);
+ };
 
-
-window.onload = function() {
-	// #1
-    var collectionContainer = document.getElementsByClassName('album-covers')[0];
-    // #2
-    collectionContainer.innerHTML = '';
- 
-    // #3
-    for (var i = 0; i < 12; i++) {
-    	collectionContainer.innerHTML += collectionItemTemplate;
+$(window).load(function() {
+    var $collectionContainer = $('.album-covers');
+    $collectionContainer.empty();
+	
+    for (var i = 0; i < 12; i++) {       
+		var $newThumbnail = buildCollectionItemTemplate();
+		$collectionContainer.append($newThumbnail);
     }
-}
+});
